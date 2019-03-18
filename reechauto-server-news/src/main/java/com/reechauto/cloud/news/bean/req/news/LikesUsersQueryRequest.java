a5f656ab1e3369b7475ebc3c9d5623d51b10731c
jinglili <jinglili@lvchiauto.com>
2019-03-18 16:10:10 +0800

    。。

--- reechauto-server-news/src/main/java/com/reechauto/cloud/news/bean/req/news/LikesUsersQueryRequest.java
+++ reechauto-server-news/src/main/java/com/reechauto/cloud/news/bean/req/news/LikesUsersQueryRequest.java
@@ -10,15 +10,12 @@ public class LikesUsersQueryRequest extends BaseRequest {
 	
 	private static final long serialVersionUID = 1L;
 
-//	@ApiModelProperty(value = "资讯动态唯一ID", dataType = "String", required = true)
 	@NotBlank(message = "资讯动态唯一ID不可以为空")
 	private String newsShareId;
 
-//	@ApiModelProperty(value = "显示条数", dataType = "int", required = true)
 	@Min(value = 1, message = "显示条数不能小于1")
 	private int pageNum = 10;
 
-//	@ApiModelProperty(value = "开始位置", dataType = "int", required = true)
 	@Min(value = 0, message = "开始位置不能小于0")
 	private int start = 0;
 
